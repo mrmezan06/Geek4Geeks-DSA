7fdf8599b29f513ed338a7c81b681dfc3fe1ead1
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-16 05:06:43 +0600

    Create Pattern12.java

---
+++ Pattern12/Pattern12.java
@@ -0,0 +1,33 @@
+package Pattern12;
+
+// https://practice.geeksforgeeks.org/problems/double-triangle-pattern-1662664259/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_12
+
+class Solution {
+
+    void printTriangle(int n) {
+        int sp = (n - 1) * 2;
+        int nm = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+            for (int j = 1; j <= sp; j++) {
+                System.out.print("  ");
+            }
+            for (int j = i; j > 0; j--) {
+                System.out.print(j + " ");
+            }
+            nm++;
+            sp -= 2;
+            System.out.println();
+        }
+    }
+}
+
+public class Pattern12 {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int n = 5;
+        obj.printTriangle(n);
+    }
+}
