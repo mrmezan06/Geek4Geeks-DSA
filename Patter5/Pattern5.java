8d635e017c0b5f4d583271b1ec508f7d9efbf44b
Mejanur Rahman Mezan <43064536+mrmezan06@users.noreply.github.com>
2022-09-16 04:50:44 +0600

    Create Pattern5.java

---
+++ Patter5/Pattern5.java
@@ -0,0 +1,24 @@
+package Patter5;
+// Link:
+
+// https://practice.geeksforgeeks.org/problems/triangle-pattern/1?utm_source=youtube&utm_medium=collab_striver_ytdescription&utm_campaign=pattern_5
+
+class Solution {
+
+    void printTriangle(int n) {
+        for (int i = n; i > 0; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+public class Pattern5 {
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+        int n = 5;
+        obj.printTriangle(n);
+    }
+}
